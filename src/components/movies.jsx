1c370bb321c9ed2cb1b602322c7bf73133946ac9
Marek Pradid <marek.pradid@telekom.com>
2022-06-09 13:11:17 +0200

    fixing warning

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef} from "react";
 import './movies.scss';
 import {Pagination} from "./common/pagination";
 import {PaginationStep} from "../utils/paginationSteps";
